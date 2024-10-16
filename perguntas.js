4563a56e2dd8dbc271ca5e9e7e988d9b654b1f0b
sweetjuhlia <162701239+sweetjuhlia@users.noreply.github.com>
2024-10-16 09:18:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é html?',
+    'É o bloco de construção mais básico da web. Define o significado e a estrutura do conteúdo da web.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são Células?',
+    'A célula é a unidade estrutural e funcional dos seres vivos, que podem ser formados por uma única célula, no caso dos unicelulares, ou várias células associadas, os multicelulares.'
+)
+
+criaCartao(
+    'Hitória',
+    'O que é Iluminismo?',
+    'O Iluminismo se iniciou como um movimento cultural europeu do século XVII e XVIII que buscava gerar mudanças políticas, econômicas e sociais na sociedade da época.'
+)
+
+criaCartao(
+    'Hora da piada',
+    'O boi e a vaca foram passar a noite em um hotel. Quem pagou?',
+    'Os dois fizeram uma VAQUINHA!'
+)
